447bfda36b2dd92887a10346c249cf4e81a0b59e
aarushsudamalla <77029396+aarushsudamalla@users.noreply.github.com>
2021-01-17 21:19:51 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ function setup() {
   
   trex.addAnimation("running", trex_running);
   trex.addAnimation("collided", trex_collided);
-  trex.scale = 0.01;
+  trex.scale = 0.2;
   
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
@@ -53,9 +53,9 @@ function setup() {
   restart = createSprite(300,140);
   restart.addImage(restartImg);
   
-  gameOver.scale = 0.01;
-  restart.scale = 0.01;
-  ground.scale = 0.01;
+  gameOver.scale = 0.2;
+  restart.scale = 0.2;
+  ground.scale = 0.2;
 
   gameOver.visible = false;
   restart.visible = false;
@@ -128,7 +128,7 @@ function spawnClouds() {
     var cloud = createSprite(600,120,40,10);
     cloud.y = Math.round(random(80,120));
     cloud.addImage(cloudImage);
-    cloud.scale = 0.01;
+    cloud.scale = 0.2;
     cloud.velocityX = -3;
     
      //assign lifetime to the variable
@@ -169,7 +169,7 @@ function spawnObstacles() {
     }
     
     //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.01;
+    obstacle.scale = 0.2;
     obstacle.lifetime = 300;
     //add each obstacle to the group
     obstaclesGroup.add(obstacle);
