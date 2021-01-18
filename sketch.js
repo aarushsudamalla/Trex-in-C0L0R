3106beaf689b0d1e8bdc926ea3ef0056e6ecf9a3
aarushsudamalla <77029396+aarushsudamalla@users.noreply.github.com>
2021-01-18 13:45:17 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -44,7 +44,7 @@ function setup() {
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   ground.velocityX = -(6 + 3*score/100);
-  ground.scale = 0.05
+  ground.scale = 0.08;
   
   gameOver = createSprite(width/2,height/2- 50);
   gameOver.addImage(gameOverImg);
